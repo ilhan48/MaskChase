4b4cfcac635503171571b85cf6d1915c324ede24
ilhan48 <58470523+ilhan48@users.noreply.github.com>
2021-02-01 04:49:18 +0300

    Add project files.

---
+++ Business/Concrete/PTTManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PTTManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+        
+
+        public PTTManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+
+        }
+        public void GiveMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " gived mask.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " not gived mask.");
+            }
+
+        }
+    }
+
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "İLHAN";
+            person1.LastName = "ÖDÜN";
+            person1.DateofBirthYear = 1999;
+            person1.NationalIdentity = 1234;
+
+
+
+            PTTManager pTTManager = new PTTManager(new PersonManager());
+            pTTManager.GiveMask(person1);
+            Console.ReadLine();
+        }
+
+    }
+}
+
+//    public class Person
+//    {
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public int DateofBorn { get; set; }
+//        public long NationalId { get; set; }
+//    }
+//}
